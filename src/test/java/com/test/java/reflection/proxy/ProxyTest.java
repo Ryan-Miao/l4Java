08f9ad13a8d236d92d67803dfece7efd2ccceb2c
Ryan Miao <woshimrf@126.com>
2019-06-25 20:57:52 +0800

    proxy test

---
+++ src/test/java/com/test/java/reflection/proxy/ProxyTest.java
@@ -0,0 +1,38 @@
+package com.test.java.reflection.proxy;
+
+import org.junit.Test;
+
+import java.io.Serializable;
+import java.lang.reflect.InvocationHandler;
+import java.lang.reflect.Proxy;
+import java.util.Arrays;
+import java.util.Random;
+
+/**
+ * Test proxy class
+ *
+ * @author Ryan
+ */
+public class ProxyTest {
+
+    @Test
+    public void traceBinarySearch() {
+        Object[] elements = new Object[1000];
+
+        for (int i = 0; i < elements.length; i++) {
+            Integer value = i + 1;
+            InvocationHandler handler = new TraceHandler(value);
+            Object proxy = Proxy.newProxyInstance(null, new Class[]{Comparable.class}, handler);
+            elements[i] = proxy;
+        }
+
+        Integer key = new Random().nextInt(elements.length) + 1;
+
+        int result = Arrays.binarySearch(elements, key);
+
+        if (result > 0) {
+            System.out.println(elements[result]);
+        }
+    }
+
+}
---
+++ src/test/java/com/test/java/reflection/proxy/TraceHandler.java
@@ -0,0 +1,36 @@
+package com.test.java.reflection.proxy;
+
+import java.lang.reflect.InvocationHandler;
+import java.lang.reflect.Method;
+
+/**
+ * trace method name and param
+ */
+public class TraceHandler implements InvocationHandler {
+
+    private Object target;
+
+    public TraceHandler(Object target) {
+        this.target = target;
+    }
+
+    @Override
+    public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
+
+        System.out.print(target);
+        System.out.print("." + method.getName() + "(");
+
+        if (args != null) {
+            for (int i = 0; i < args.length; i++) {
+                System.out.print(args[i]);
+                if (i<args.length - 1){
+                    System.out.print(", ");
+                }
+            }
+        }
+
+        System.out.println(")");
+
+        return method.invoke(target, args);
+    }
+}
