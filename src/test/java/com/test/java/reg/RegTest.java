f90561359b75f20d7365b3af0d70fe23d4654b83
Ryan Miao <ryan.miao@nf-3.com>
2019-12-12 11:20:52 +0800

    mobile reg

--- src/test/java/com/test/java/reg/RegTest.java
+++ src/test/java/com/test/java/reg/RegTest.java
@@ -2,6 +2,8 @@ package com.test.java.reg;
 
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
+
+import org.junit.Assert;
 import org.junit.Test;
 
 /**
@@ -16,6 +18,15 @@ public class RegTest {
         Matcher matcher = pattern.matcher("ROLE_aaa");
         boolean matches = matcher.matches();
         System.out.println(matches);
+    }
+
+    @Test
+    public void testMobile() {
+        String mobile = "18680331512";
+        String reg =  "^1[3|4|5|7|8]\\d{9}$";
+        Pattern pattern = Pattern.compile(reg);
+        boolean matches = pattern.matcher(mobile).matches();
+        Assert.assertTrue(matches);
     }
 
 }
